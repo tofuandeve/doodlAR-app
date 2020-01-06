9ea3782254ebd4499c5f8e9cc849baffd20ce140
Eve Le <nguyenthao.lee@gmail.com>
2020-01-06 03:06:01 -0800

    Implemented GridImage class to display grid indicating a surface is detected

--- doodlAR/GridImage.swift
+++ doodlAR/GridImage.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+import SceneKit
+import ARKit
+
+class GridImage : SCNNode {
+    
+    var anchor: ARPlaneAnchor
+    var planeGeometry: SCNPlane!
+    
+    init(anchor: ARPlaneAnchor) {
+        self.anchor = anchor
+        super.init()
+        
+        // set the plane size
+        planeGeometry = SCNPlane(width: CGFloat(self.anchor.extent.x), height: CGFloat(self.anchor.extent.z))
+        // set plane texture
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named:"overlay_grid.png")
+        
+        planeGeometry.materials = [material]
+        
+        let planeNode = SCNNode(geometry: self.planeGeometry)
+        planeNode.physicsBody = SCNPhysicsBody(type: .static, shape: SCNPhysicsShape(geometry: self.planeGeometry, options: nil))
+        
+        // TODO: read about this categoryBitMask
+        planeNode.physicsBody?.categoryBitMask = 2
+        
+        planeNode.position = SCNVector3Make(anchor.center.x, 0, anchor.center.z);
+        planeNode.transform = SCNMatrix4MakeRotation(Float(-Double.pi / 2.0), 1.0, 0.0, 0.0);
+
+        addChildNode(planeNode)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+ 
+}
