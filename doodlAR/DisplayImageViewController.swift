ecbaffc2b10ddd7a39cddc0fd8ae736fe3503a38
Eve Le <nguyenthao.lee@gmail.com>
2020-01-12 22:46:41 -0800

    Added DisplayImageViewController

---
+++ doodlAR/DisplayImageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DisplayImageViewController.swift
+//  doodlAR
+//
+//  Created by Eve Le on 1/12/20.
+//  Copyright © 2020 Eve Le. All rights reserved.
+//
+
+import UIKit
+
+class DisplayImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
