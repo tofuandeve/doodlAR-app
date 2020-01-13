2702f5d8ecea8a980d73c206ac33fc706a6872e4
Eve Le <nguyenthao.lee@gmail.com>
2020-01-12 22:49:29 -0800

    Implemented DisplayImageViewController

--- doodlAR/DisplayImageViewController.swift
+++ doodlAR/DisplayImageViewController.swift
@@ -10,21 +10,18 @@ import UIKit
 
 class DisplayImageViewController: UIViewController {
 
+    @IBOutlet weak var displayImageView: UIImageView!
+    var displayImage: UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.displayImageView.image = displayImage
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func onDrawButtonTapped(_ sender: RoundButton) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(identifier: "ARViewController") as! ARViewController
+        vc.displayImage = self.displayImage
+        self.navigationController!.pushViewController(vc, animated: true)
     }
-    */
-
 }
