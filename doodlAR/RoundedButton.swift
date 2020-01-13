ba227ec3c04c75653d53d5cf03636cd66515bda6
Eve Le <nguyenthao.lee@gmail.com>
2020-01-12 22:49:07 -0800

    Added RoundedButton class for button

---
+++ doodlAR/RoundedButton.swift
@@ -0,0 +1,32 @@
+//
+//  RoundedButton.swift
+//  doodlAR
+//
+//  Created by Eve Le on 1/12/20.
+//  Copyright © 2020 Eve Le. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
